ce7a444302b29edf0f92790e91c8f5c293dd0845
Mitul Gautam <mgautam@birchstreet.net>
2026-03-06 17:56:06 +0530

    Initial commit

---
+++ src/main/java/dev/mitulgautam/pixel_flow/PixelFlowApplication.java
@@ -0,0 +1,13 @@
+package dev.mitulgautam.pixel_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mitulgautam/pixel_flow/PixelFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mitulgautam.pixel_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
